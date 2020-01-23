2d6e4587c84593f6aa758e21263f02662b6162be
Joshua Oguma <joshua_oguma@epuakhaw0361.kyiv.epam.com>
2020-01-23 13:02:59 +0200

    Conculsions for 22-January

--- browser-setup/fromEvent/index.ts
+++ browser-setup/fromEvent/index.ts
@@ -1,1 +1,15 @@
-console.log('Hellloooooooo')
+import { fromEvent } from "rxjs";
+import { tap, map, pluck } from "rxjs/operators";
+
+fromEvent(document.forms[0], 'submit').pipe(
+    tap(()=> console.log('Preventing Default'))
+).subscribe(e => e.preventDefault());
+
+fromEvent(document.getElementById('animal'), 'change').pipe(
+    pluck('target', 'value')
+)
+.subscribe(e => console.log(e))
+
+fromEvent(document.getElementById('moto'), 'change').pipe(
+    pluck('target', 'value')
+).subscribe(console.log)
---
+++ merge/index.ts
@@ -0,0 +1,21 @@
+import { interval, merge } from "rxjs";
+import { map, take } from "rxjs/operators";
+import { fromStdIn } from "../utility";
+
+// const bluePostFeed = interval(1000).pipe(
+//     map(n=> `Blue Post Article #${n}`)
+// );
+
+// const redHeraldFeed = interval(1000).pipe(
+//     map(n=> `Red Herald Article #${n}`)
+// );
+
+
+// merge(bluePostFeed, redHeraldFeed)
+// .subscribe(console.log)
+
+const clicks = fromStdIn().pipe(take(10));
+const timer = interval(1000);
+
+const clicksOrTimer = merge(clicks, timer, 1);
+clicksOrTimer.subscribe(console.log)
